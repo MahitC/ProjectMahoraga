dc2304773c1b56f1ffd75a7a9276ec94d841538e
Near <mahit.chekuri@gmail.com>
2024-10-27 01:42:31 -0400

    Create gift.js

---
+++ gift page/gift.js
@@ -0,0 +1,9 @@
+function showCard(){
+    document.getElementById("info").style.display="none";
+    document.getElementById("card").style.display="grid";
+}
+
+function showInfo(){
+    document.getElementById("card").style.display="none";
+    document.getElementById("info").style.display="grid";
+}
